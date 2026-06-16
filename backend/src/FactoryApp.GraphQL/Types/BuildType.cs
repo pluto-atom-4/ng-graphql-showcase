6081fcb66870250f7e3411c4482d9b2e69cd639d
Akai Kaede <genko2@hotmail.com>
2026-06-16 12:35:35 -0700

    feat: add BuildType GraphQL type with fields and data loaders

---
+++ backend/src/FactoryApp.GraphQL/Types/BuildType.cs
@@ -0,0 +1,38 @@
+using FactoryApp.Domain;
+using FactoryApp.Domain.Entities;
+
+namespace FactoryApp.GraphQL.Types;
+
+[GraphQLType("Build")]
+public class BuildType
+{
+    [GraphQLField]
+    public Guid Id { get; set; }
+
+    [GraphQLField]
+    public string Name { get; set; } = null!;
+
+    [GraphQLField]
+    public string? Description { get; set; }
+
+    [GraphQLField]
+    public string Status { get; set; } = null!;
+
+    [GraphQLField]
+    public DateTime CreatedAt { get; set; }
+
+    [GraphQLField]
+    public DateTime UpdatedAt { get; set; }
+
+    [GraphQLField]
+    public async Task<List<Part>> Parts(
+        [Parent] Build build,
+        [DataLoader("GetPartsByBuildId")] IDataLoader<Guid, List<Part>> loader)
+        => await loader.LoadAsync(build.Id);
+
+    [GraphQLField]
+    public async Task<List<TestRun>> TestRuns(
+        [Parent] Build build,
+        [DataLoader("GetTestRunsByBuildId")] IDataLoader<Guid, List<TestRun>> loader)
+        => await loader.LoadAsync(build.Id);
+}
