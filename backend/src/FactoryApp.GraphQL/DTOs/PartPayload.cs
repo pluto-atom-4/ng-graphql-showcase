91cf0c0098b86306c7fe61b3de4389be5f6ce023
Akai Kaede <genko2@hotmail.com>
2026-06-16 12:48:09 -0700

    feat: add PartPayload DTO for GraphQL operations

---
+++ backend/src/FactoryApp.GraphQL/DTOs/PartPayload.cs
@@ -0,0 +1,11 @@
+namespace FactoryApp.GraphQL.DTOs;
+
+public class PartPayload
+{
+    public Guid Id { get; set; }
+    public Guid BuildId { get; set; }
+    public required string Name { get; set; }
+    public required string SKU { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
